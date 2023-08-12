5fbfe463782d4cc46fa6efca977a433e5bb171c6
XenKys <sergiotuzzo@gmail.com>
2023-08-12 19:39:06 +0200

    disgroove@1.1.8

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-export * from "./Client";
 export * from "./rest";
 export * from "./structures";
 export * from "./types";
 export * from "./utils";
+export * from "./Client";
+export * from "./constants";
