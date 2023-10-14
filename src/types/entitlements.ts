15890ab45fb69a653fe536f2d4bf338415657205
XenKys <sergiotuzzo@gmail.com>
2023-10-14 19:41:02 +0200

    Replaced consumed with deleted in Entitlement interfaces

--- src/types/entitlements.ts
+++ src/types/entitlements.ts
@@ -4,23 +4,23 @@ import type { EntitlementTypes } from "../constants";
 export interface RawEntitlement {
   id: string;
   sku_id: string;
-  user_id?: string;
-  guild_id?: string;
   application_id: string;
+  user_id?: string;
   type: EntitlementTypes;
-  consumed: boolean;
+  deleted: boolean;
   starts_at?: string;
   ends_at?: string;
+  guild_id?: string;
 }
 
 export interface JSONEntitlement {
   id: string;
   skuId: string;
-  userId?: string;
-  guildId?: string;
   applicationId: string;
+  userId?: string;
   type: EntitlementTypes;
-  consumed: boolean;
+  deleted: boolean;
   startsAt?: string;
   endsAt?: string;
+  guildId?: string;
 }
