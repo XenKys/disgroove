28135d348d9dce438fada4df923cffc986cef628
XenKys <sergiotuzzo@gmail.com>
2023-12-29 19:20:34 +0100

    Updated entitlements and skus interfaces

--- src/types/entitlements.ts
+++ src/types/entitlements.ts
@@ -6,11 +6,15 @@ export interface RawEntitlement {
   sku_id: string;
   application_id: string;
   user_id?: string;
+  promotion_id?: string | null; // Undocumented
   type: EntitlementTypes;
   deleted: boolean;
+  gift_code_flags?: number; // Undocumented
+  consumed?: boolean; // Undocumented
   starts_at?: string;
   ends_at?: string;
   guild_id?: string;
+  subscription_id?: string; // Undocumented
 }
 
 export interface JSONEntitlement {
@@ -18,9 +22,13 @@ export interface JSONEntitlement {
   skuId: string;
   applicationId: string;
   userId?: string;
+  promotionId?: string | null; // Undocumented
   type: EntitlementTypes;
   deleted: boolean;
+  giftCodeFlags?: number; // Undocumented
+  consumed?: boolean; // Undocumented
   startsAt?: string;
   endsAt?: string;
   guildId?: string;
+  subscriptionId?: string; // Undocumented
 }
--- src/types/sku.ts
+++ src/types/sku.ts
@@ -4,17 +4,29 @@ import type { SKUFlags, SKUTypes } from "../constants";
 export interface RawSKU {
   id: string;
   type: SKUTypes;
+  dependent_sku_id?: string | null; // Undocumented
   application_id: string;
+  manifest_labels?: null; // Undocumented
+  access_type?: number; // Undocumented
   name: string;
+  features?: []; // Undocumented
+  release_date?: null; // Undocumented
   slug: string;
   flags: SKUFlags;
+  show_age_gate?: boolean; // Undocumented
 }
 
 export interface JSONSKU {
   id: string;
   type: SKUTypes;
+  dependentSkuId?: string | null; // Undocumented
   applicationId: string;
+  manifestLabels?: null; // Undocumented
+  accessType?: number; // Undocumented
   name: string;
+  features?: []; // Undocumented
+  releaseDate?: null; // Undocumented
   slug: string;
   flags: SKUFlags;
+  showAgeGate?: boolean; // Undocumented
 }
