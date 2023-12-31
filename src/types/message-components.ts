be5c0b651ec03de4134f8961e8f9d8c4f5683a35
XenKys <sergiotuzzo@gmail.com>
2023-12-31 11:03:31 +0100

    Improved action row types

--- src/types/message-components.ts
+++ src/types/message-components.ts
@@ -66,44 +66,7 @@ export interface RawTextInput {
 /** https://discord.com/developers/docs/interactions/message-components#action-rows */
 export interface RawActionRow {
   type: ComponentTypes.ActionRow;
-  components: Array<
-    | {
-        type: ComponentTypes.Button;
-        style: ButtonStyles;
-        label?: string;
-        emoji?: Partial<Pick<RawEmoji, "name" | "id" | "animated">>;
-        custom_id?: string;
-        url?: string;
-        disabled?: boolean;
-      }
-    | {
-        type:
-          | ComponentTypes.ChannelSelect
-          | ComponentTypes.MentionableSelect
-          | ComponentTypes.RoleSelect
-          | ComponentTypes.StringSelect
-          | ComponentTypes.UserSelect;
-        custom_id: string;
-        options?: Array<RawSelectOption>;
-        channel_types?: Array<ChannelTypes>;
-        placeholder?: string;
-        default_values?: Array<RawDefaultValue>;
-        min_values?: number;
-        max_values?: number;
-        disabled?: boolean;
-      }
-    | {
-        type: ComponentTypes.TextInput;
-        custom_id: string;
-        style: TextInputStyles;
-        label: string;
-        min_length?: number;
-        max_length?: number;
-        required?: boolean;
-        value?: string;
-        placeholder?: string;
-      }
-  >;
+  components: Array<RawButton | RawSelectMenu | RawTextInput>;
 }
 
 export interface JSONButton {
@@ -160,42 +123,5 @@ export interface JSONTextInput {
 
 export interface JSONActionRow {
   type: ComponentTypes.ActionRow;
-  components: Array<
-    | {
-        type: ComponentTypes.Button;
-        style: ButtonStyles;
-        label?: string;
-        emoji?: Partial<Pick<JSONEmoji, "name" | "id" | "animated">>;
-        customId?: string;
-        url?: string;
-        disabled?: boolean;
-      }
-    | {
-        type:
-          | ComponentTypes.ChannelSelect
-          | ComponentTypes.MentionableSelect
-          | ComponentTypes.RoleSelect
-          | ComponentTypes.StringSelect
-          | ComponentTypes.UserSelect;
-        customId: string;
-        options?: Array<JSONSelectOption>;
-        channelTypes?: Array<ChannelTypes>;
-        placeholder?: string;
-        defaultValues?: Array<JSONDefaultValue>;
-        minValues?: number;
-        maxValues?: number;
-        disabled?: boolean;
-      }
-    | {
-        type: ComponentTypes.TextInput;
-        customId: string;
-        style: TextInputStyles;
-        label: string;
-        minLength?: number;
-        maxLength?: number;
-        required?: boolean;
-        value?: string;
-        placeholder?: string;
-      }
-  >;
+  components: Array<JSONButton | JSONSelectMenu | JSONTextInput>;
 }
