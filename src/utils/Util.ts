44e877724e935f391053bdf4089c42efc287f8ee
XenKys <sergiotuzzo@gmail.com>
2024-01-03 21:23:41 +0100

    Fixed Util functions

--- src/utils/Util.ts
+++ src/utils/Util.ts
@@ -1,69 +1,30 @@
 import type {
   JSONActionRow,
-  JSONApplicationCommandOptionChoice,
-  JSONDefaultReaction,
+  JSONApplicationCommand,
+  JSONChannel,
   JSONEmbed,
   JSONEmoji,
-  JSONForumTag,
-  JSONOverwrite,
-  JSONRoleTags,
-  JSONSelectOption,
-  JSONThreadMember,
-  JSONThreadMetadata,
+  JSONRole,
   JSONUser,
+  RawActionRow,
+  RawApplicationCommand,
+  RawChannel,
+  RawEmbed,
+  RawEmoji,
+  RawRole,
+  RawUser,
 } from "../types";
-import {
-  type ApplicationCommandOptionType,
-  type ChannelTypes,
-  ComponentTypes,
-  type Locale,
-  type UserFlags,
-} from "../constants";
+import { ComponentTypes } from "../constants";
 
 export class Util {
-  applicationCommandToRaw(command: {
-    id?: string;
-    name?: string;
-    nameLocalizations?: Partial<Record<Locale, string>> | null;
-    description?: string;
-    descriptionLocalizations?: Partial<Record<Locale, string>> | null;
-    options?: Array<{
-      type: ApplicationCommandOptionType;
-      name: string;
-      nameLocalizations?: Partial<Record<Locale, string>>;
-      description: string;
-      descriptionLocalizations?: Partial<Record<Locale, string>>;
-      required?: boolean;
-      choices?: Array<JSONApplicationCommandOptionChoice>;
-      options?: Array<{
-        type: ApplicationCommandOptionType;
-        name: string;
-        nameLocalizations?: Partial<Record<Locale, string>>;
-        description: string;
-        descriptionLocalizations?: Partial<Record<Locale, string>>;
-        required?: boolean;
-        choices?: Array<JSONApplicationCommandOptionChoice>;
-        channelTypes?: Array<ChannelTypes>;
-        minValue?: number;
-        maxValue?: number;
-        minLength?: number;
-        maxLength?: number;
-        autocomplete?: boolean;
-      }>;
-      channelTypes?: Array<ChannelTypes>;
-      minValue?: number;
-      maxValue?: number;
-      minLength?: number;
-      maxLength?: number;
-      autocomplete?: boolean;
-    }>;
-    defaultMemberPermissions?: string | null;
-    defaultPermission?: boolean | null;
-    dmPermission?: boolean;
-    nsfw?: boolean;
-  }) {
+  applicationCommandToRaw(
+    command: JSONApplicationCommand
+  ): RawApplicationCommand {
     return {
       id: command.id,
+      type: command.type,
+      application_id: command.applicationId,
+      guild_id: command.guildId,
       name: command.name,
       name_localizations: command.nameLocalizations,
       description: command.description,
@@ -107,12 +68,189 @@ export class Util {
         autocomplete: option.autocomplete,
       })),
       default_member_permissions: command.defaultMemberPermissions,
+      dm_permission: command.dmPermission,
       default_permission: command.defaultPermission,
       nsfw: command.nsfw,
+      version: command.version,
     };
   }
 
-  messageComponentToRaw(components: Array<JSONActionRow>) {
+  channelToRaw(channel: JSONChannel): RawChannel {
+    return {
+      id: channel.id,
+      type: channel.type,
+      guild_id: channel.guildId,
+      position: channel.position,
+      permission_overwrites: channel.permissionOverwrites?.map((overwrite) => ({
+        id: overwrite.id,
+        type: overwrite.type,
+        allow: overwrite.allow,
+        deny: overwrite.deny,
+      })),
+      name: channel.name,
+      topic: channel.topic,
+      nsfw: channel.nsfw,
+      last_message_id: channel.lastMessageId,
+      bitrate: channel.bitrate,
+      user_limit: channel.userLimit,
+      rate_limit_per_user: channel.rateLimitPerUser,
+      recipients: channel.recipients?.map((recipient) =>
+        this.userToRaw(recipient)
+      ),
+      icon: channel.icon,
+      owner_id: channel.ownerId,
+      application_id: channel.applicationId,
+      managed: channel.managed,
+      parent_id: channel.parentId,
+      last_pin_timestamp: channel.lastPinTimestamp,
+      rtc_region: channel.rtcRegion,
+      video_quality_mode: channel.videoQualityMode,
+      message_count: channel.messageCount,
+      member_count: channel.memberCount,
+      thread_metadata:
+        channel.threadMetadata !== undefined
+          ? {
+              archived: channel.threadMetadata?.archived,
+              auto_archive_duration:
+                channel.threadMetadata?.autoArchiveDuration,
+              archive_timestamp: channel.threadMetadata?.archiveTimestamp,
+              locked: channel.threadMetadata?.locked,
+              invitable: channel.threadMetadata?.invitable,
+              create_timestamp: channel.threadMetadata?.createTimestamp,
+            }
+          : undefined,
+      member:
+        channel.member !== undefined
+          ? {
+              id: channel.member.id,
+              user_id: channel.member.userId,
+              join_timestamp: channel.member.joinTimestamp,
+              flags: channel.member.flags,
+              member:
+                channel.member.member !== undefined
+                  ? {
+                      user:
+                        channel.member.member.user !== undefined
+                          ? this.userToRaw(channel.member.member.user)
+                          : undefined,
+                      nick: channel.member.member.nick,
+                      avatar: channel.member.member.avatar,
+                      roles: channel.member.member.roles,
+                      joined_at: channel.member.member.joinedAt,
+                      premium_since: channel.member.member.premiumSince,
+                      deaf: channel.member.member.deaf,
+                      mute: channel.member.member.mute,
+                      flags: channel.member.member.flags,
+                      pending: channel.member.member.pending,
+                      permissions: channel.member.member.permissions,
+                      communication_disabled_until:
+                        channel.member.member.communicationDisabledUntil,
+                    }
+                  : undefined,
+            }
+          : undefined,
+      default_auto_archive_duration: channel.defaultAutoArchiveDuration,
+      permissions: channel.permissions,
+      flags: channel.flags,
+      total_message_sent: channel.totalMessageSent,
+      available_tags: channel.availableTags?.map((availableTag) => ({
+        id: availableTag.id,
+        name: availableTag.name,
+        moderated: availableTag.moderated,
+        emoji_id: availableTag.emojiId,
+        emoji_name: availableTag.emojiName,
+      })),
+      applied_tags: channel.appliedTags,
+      default_reaction_emoji:
+        channel.defaultReactionEmoji !== undefined
+          ? channel.defaultReactionEmoji !== null
+            ? {
+                emoji_id: channel.defaultReactionEmoji.emojiId,
+                emoji_name: channel.defaultReactionEmoji.emojiName,
+              }
+            : null
+          : undefined,
+      default_thread_rate_limit_per_user: channel.defaultThreadRateLimitPerUser,
+      default_sort_order: channel.defaultSortOrder,
+      default_forum_layout: channel.defaultForumLayout,
+    };
+  }
+
+  embedsToRaw(embeds: Array<JSONEmbed>): Array<RawEmbed> {
+    return embeds.map((embed) => ({
+      title: embed.title,
+      type: embed.type,
+      description: embed.description,
+      url: embed.url,
+      timestamp: embed.timestamp,
+      color: embed.color,
+      footer:
+        embed.footer !== undefined
+          ? {
+              text: embed.footer.text,
+              icon_url: embed.footer.iconUrl,
+              proxy_icon_url: embed.footer.proxyIconUrl,
+            }
+          : undefined,
+      image:
+        embed.image !== undefined
+          ? {
+              url: embed.image.url,
+              proxy_url: embed.image.proxyUrl,
+              height: embed.image.height,
+              width: embed.image.width,
+            }
+          : undefined,
+      thumbnail:
+        embed.thumbnail !== undefined
+          ? {
+              url: embed.thumbnail.url,
+              proxy_url: embed.thumbnail.proxyUrl,
+              height: embed.thumbnail.height,
+              width: embed.thumbnail.width,
+            }
+          : undefined,
+      video: {
+        url: embed.video?.url,
+        proxy_url: embed.video?.proxyUrl,
+        height: embed.video?.height,
+        width: embed.video?.width,
+      },
+      provider: {
+        name: embed.provider?.name,
+        url: embed.provider?.url,
+      },
+      author:
+        embed.author !== undefined
+          ? {
+              name: embed.author.name,
+              url: embed.author.url,
+              icon_url: embed.author.iconUrl,
+              proxy_icon_url: embed.author.proxyIconUrl,
+            }
+          : undefined,
+      fields: embed.fields?.map((field) => ({
+        name: field.name,
+        value: field.value,
+        inline: field.inline,
+      })),
+    }));
+  }
+
+  emojiToRaw(emoji: JSONEmoji): RawEmoji {
+    return {
+      id: emoji.id,
+      name: emoji.name,
+      roles: emoji.roles,
+      user: emoji.user !== undefined ? this.userToRaw(emoji.user) : undefined,
+      require_colons: emoji.requireColons,
+      managed: emoji.managed,
+      animated: emoji.animated,
+      available: emoji.available,
+    };
+  }
+
+  messageComponentToRaw(components: Array<JSONActionRow>): Array<RawActionRow> {
     return components.map((component) => ({
       type: component.type,
       components: component.components.map((c) => {
@@ -154,6 +292,7 @@ export class Util {
               custom_id: c.customId,
               channel_types: c.channelTypes,
               placeholder: c.placeholder,
+              default_values: c.defaultValues,
               min_values: c.minValues,
               max_values: c.maxValues,
               disabled: c.disabled,
@@ -164,6 +303,7 @@ export class Util {
               type: c.type,
               custom_id: c.customId,
               placeholder: c.placeholder,
+              default_values: c.defaultValues,
               options: c.options?.map((option) => ({
                 label: option.label,
                 value: option.value,
@@ -190,6 +330,7 @@ export class Util {
               type: c.type,
               custom_id: c.customId,
               placeholder: c.placeholder,
+              default_values: c.defaultValues,
               min_values: c.minValues,
               max_values: c.maxValues,
               disabled: c.disabled,
@@ -200,22 +341,8 @@ export class Util {
     }));
   }
 
-  roleToRaw(role: {
-    guildId?: string;
-    id?: string;
-    name?: string;
-    color?: number;
-    hoist?: boolean;
-    icon?: string | null;
-    unicodeEmoji?: string | null;
-    position?: number;
-    permissions?: string;
-    managed?: boolean;
-    mentionable?: boolean;
-    tags?: JSONRoleTags;
-  }) {
+  roleToRaw(role: JSONRole): RawRole {
     return {
-      guild_id: role.guildId,
       id: role.id,
       name: role.name,
       color: role.color,
@@ -234,27 +361,11 @@ export class Util {
         available_for_purchase: role.tags?.availableForPurchase,
         guild_connections: role.tags?.guildConnections,
       },
+      flags: role.flags,
     };
   }
 
-  userToRaw(user: {
-    id?: string;
-    username?: string;
-    discriminator?: string;
-    globalName?: string | null;
-    avatar?: string;
-    bot?: boolean;
-    system?: boolean;
-    mfaEnabled?: boolean;
-    banner?: string;
-    accentColor?: number;
-    locale?: string;
-    verified?: boolean;
-    email?: string;
-    flags?: UserFlags;
-    premiumType?: number;
-    publicFlags?: UserFlags;
-  }) {
+  userToRaw(user: JSONUser): RawUser {
     return {
       id: user.id,
       username: user.username,
@@ -272,198 +383,7 @@ export class Util {
       flags: user.flags,
       premium_type: user.premiumType,
       public_flags: user.publicFlags,
-    };
-  }
-
-  channelToRaw(channel: {
-    id?: string;
-    type?: ChannelTypes;
-    guildId?: string;
-    position?: number;
-    permissionOverwrites?: Array<JSONOverwrite>;
-    name?: string | null;
-    topic?: string | null;
-    nsfw?: boolean;
-    lastMessageId?: string | null;
-    bitrate?: number;
-    userLimit?: number;
-    rateLimitPerUser?: number;
-    recipients?: Array<JSONUser>;
-    icon?: string | null;
-    ownerId?: string;
-    applicationId?: string;
-    managed?: boolean;
-    parentId?: string | null;
-    lastPinTimestamp?: string | null;
-    rtcRegion?: string | null;
-    videoQualityMode?: number;
-    messageCount?: number;
-    memberCount?: number;
-    threadMetadata?: JSONThreadMetadata;
-    member?: JSONThreadMember;
-    defaultAutoArchiveDuration?: number;
-    permissions?: string;
-    flags?: number;
-    totalMessageSent?: number;
-    availableTags?: Array<JSONForumTag>;
-    appliedTags?: Array<string>;
-    defaultReactionEmoji?: JSONDefaultReaction | null;
-    defaultThreadRateLimitPerUser?: number;
-    defaultSortOrder?: number | null;
-    defaultForumLayout?: number;
-  }) {
-    return {
-      id: channel.id,
-      type: channel.type,
-      guild_id: channel.guildId,
-      position: channel.position,
-      permission_overwrites: channel.permissionOverwrites?.map((overwrite) => ({
-        id: overwrite.id,
-        type: overwrite.type,
-        allow: overwrite.allow,
-        deny: overwrite.deny,
-      })),
-      name: channel.name,
-      topic: channel.topic,
-      nsfw: channel.nsfw,
-      last_message_id: channel.lastMessageId,
-      bitrate: channel.bitrate,
-      user_limit: channel.userLimit,
-      rate_limit_per_user: channel.rateLimitPerUser,
-      recipients: channel.recipients?.map((recipient) =>
-        this.userToRaw(recipient)
-      ),
-      icon: channel.icon,
-      owner_id: channel.ownerId,
-      application_id: channel.applicationId,
-      managed: channel.managed,
-      parent_id: channel.parentId,
-      last_pin_timestamp: channel.lastPinTimestamp,
-      rtc_region: channel.rtcRegion,
-      video_quality_mode: channel.videoQualityMode,
-      message_count: channel.messageCount,
-      member_count: channel.memberCount,
-      thread_metadata: {
-        archived: channel.threadMetadata?.archived,
-        auto_archive_duration: channel.threadMetadata?.autoArchiveDuration,
-        archive_timestamp: channel.threadMetadata?.archiveTimestamp,
-        locked: channel.threadMetadata?.locked,
-        invitable: channel.threadMetadata?.invitable,
-        create_timestamp: channel.threadMetadata?.createTimestamp,
-      },
-      member: {
-        id: channel.member?.id,
-        user_id: channel.member?.userId,
-        join_timestamp: channel.member?.joinTimestamp,
-        flags: channel.member?.flags,
-        member: {
-          user:
-            channel.member?.member?.user !== undefined
-              ? this.userToRaw(channel.member.member.user)
-              : undefined,
-          nick: channel.member?.member?.nick,
-          avatar: channel.member?.member?.avatar,
-          roles: channel.member?.member?.roles,
-          joined_at: channel.member?.member?.joinedAt,
-          premium_since: channel.member?.member?.premiumSince,
-          deaf: channel.member?.member?.deaf,
-          mute: channel.member?.member?.mute,
-          flags: channel.member?.member?.flags,
-          pending: channel.member?.member?.pending,
-          permissions: channel.member?.member?.permissions,
-          communication_disabled_until:
-            channel.member?.member?.communicationDisabledUntil,
-        },
-      },
-      default_auto_archive_duration: channel.defaultAutoArchiveDuration,
-      permissions: channel.permissions,
-      flags: channel.flags,
-      total_message_sent: channel.totalMessageSent,
-      available_tags: channel.availableTags?.map((availableTag) => ({
-        id: availableTag.id,
-        name: availableTag.name,
-        moderated: availableTag.moderated,
-        emoji_id: availableTag.emojiId,
-        emoji_name: availableTag.emojiName,
-      })),
-      applied_tags: channel.appliedTags,
-      default_reaction_emoji: {
-        emoji_id: channel.defaultReactionEmoji?.emojiId,
-        emoji_name: channel.defaultReactionEmoji?.emojiName,
-      },
-      default_thread_rate_limit_per_user: channel.defaultThreadRateLimitPerUser,
-      default_sort_order: channel.defaultSortOrder,
-      default_forum_layout: channel.defaultForumLayout,
-    };
-  }
-
-  embedsToRaw(embeds: Array<JSONEmbed>) {
-    return embeds.map((embed) => ({
-      title: embed.title,
-      type: embed.type,
-      description: embed.description,
-      url: embed.url,
-      timestamp: embed.timestamp,
-      color: embed.color,
-      footer: embed.footer
-        ? {
-            text: embed.footer.text,
-            icon_url: embed.footer.iconUrl,
-            proxy_icon_url: embed.footer.proxyIconUrl,
-          }
-        : undefined,
-      image: embed.image
-        ? {
-            url: embed.image.url,
-            proxy_url: embed.image.proxyUrl,
-            height: embed.image.height,
-            width: embed.image.width,
-          }
-        : undefined,
-      thumbnail: embed.thumbnail
-        ? {
-            url: embed.thumbnail.url,
-            proxy_url: embed.thumbnail.proxyUrl,
-            height: embed.thumbnail.height,
-            width: embed.thumbnail.width,
-          }
-        : undefined,
-      video: {
-        url: embed.video?.url,
-        proxy_url: embed.video?.proxyUrl,
-        height: embed.video?.height,
-        width: embed.video?.width,
-      },
-      provider: {
-        name: embed.provider?.name,
-        url: embed.provider?.url,
-      },
-      author: embed.author
-        ? {
-            name: embed.author.name,
-            url: embed.author.url,
-            icon_url: embed.author.iconUrl,
-            proxy_icon_url: embed.author.proxyIconUrl,
-          }
-        : undefined,
-      fields: embed.fields?.map((field) => ({
-        name: field.name,
-        value: field.value,
-        inline: field.inline,
-      })),
-    }));
-  }
-
-  emojiToRaw(emoji: JSONEmoji) {
-    return {
-      id: emoji.id,
-      name: emoji.name,
-      roles: emoji.roles,
-      user: emoji.user !== undefined ? this.userToRaw(emoji.user) : undefined,
-      require_colons: emoji.requireColons,
-      managed: emoji.managed,
-      animated: emoji.animated,
-      available: emoji.available,
+      avatar_decoration: user.avatarDecoration,
     };
   }
 }
