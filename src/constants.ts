a263999196912a19aca9f61361d2d217c17c470a
XenKys <sergiotuzzo@gmail.com>
2024-01-03 11:02:06 +0100

    Updated ApplicationFlags

--- src/constants.ts
+++ src/constants.ts
@@ -122,6 +122,7 @@ export enum TextInputStyles {
 
 /** https://discord.com/developers/docs/resources/application#application-object-application-flags */
 export enum ApplicationFlags {
+  ApplicationAutoModerationRuleCreateBadge = 1 << 6,
   GatewayPresence = 1 << 12,
   GatewayPresenceLimited = 1 << 13,
   GatewayGuildMembers = 1 << 14,
