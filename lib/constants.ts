85605c1da8ea082bc845ed066c450facda84b25f
XenKys <sergiotuzzo@gmail.com>
2024-02-27 17:11:11 +0100

    refactor: renamed 'SpanishLatam' to 'SpanishLATAM' in Locale constant

--- lib/constants.ts
+++ lib/constants.ts
@@ -16,7 +16,7 @@ export enum Locale {
   EnglishGB = "en-GB",
   EnglishUS = "en-US",
   SpanishES = "es-ES",
-  SpanishLatam = "es-419",
+  SpanishLATAM = "es-419",
   French = "fr",
   Croatian = "hr",
   Italian = "it",
