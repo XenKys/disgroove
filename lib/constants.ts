2892c1f483a6d52f707e46ae8684a05d62d8ade7
XenKys <sergiotuzzo@gmail.com>
2024-06-08 11:00:02 +0200

    refactor: updated MessageTypes constant

--- lib/constants.ts
+++ lib/constants.ts
@@ -333,6 +333,10 @@ export enum MessageTypes {
   StageSpeaker,
   StageTopic = 31,
   GuildApplicationPremiumSubscription,
+  GuildIncidentAlertModeEnabled = 36,
+  GuildIncidentAlertModeDisabled,
+  GuildIncidentReportRaid,
+  GuildIncidentReportFalseAlarm,
 }
 
 /** https://discord.com/developers/docs/resources/channel#message-object-message-activity-types */
