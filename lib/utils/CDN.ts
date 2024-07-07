f61e74d1d3a07ce2340260a990e0825dc7246c86
XenKys <sergiotuzzo@gmail.com>
2024-07-07 13:29:18 +0200

    refactor: improved CDN

--- lib/utils/CDN.ts
+++ lib/utils/CDN.ts
@@ -1,68 +1,197 @@
+/* https://discord.com/developers/docs/reference#image-formatting */
+
+import { ImageFormats } from "../constants";
 import type { snowflake } from "../types/common";
+
+export const cdnURL = (
+  cdnEndpoint: string,
+  imageFormat: ImageFormats = ImageFormats.PNG
+) => `https://cdn.discordapp.com/${cdnEndpoint}.${imageFormat}` as const;
 
 export const achievementIcon = (
   applicationID: snowflake,
   achievementID: snowflake,
-  iconHash: string
+  iconHash: string,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP = ImageFormats.PNG
 ) =>
-  `https://cdn.discordapp.com/app-assets/${applicationID}/achievements/${achievementID}/icons/${iconHash}.png` as const;
+  cdnURL(
+    `app-assets/${applicationID}/achievements/${achievementID}/icons/${iconHash}`,
+    imageFormat
+  );
 export const applicationAsset = (
   applicationID: snowflake,
-  assetID: snowflake
-) =>
-  `https://cdn.discordapp.com/app-assets/${applicationID}/${assetID}.png` as const;
+  assetID: snowflake,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP = ImageFormats.PNG
+) => cdnURL(`app-assets/${applicationID}/${assetID}`, imageFormat);
 export const applicationCover = (
   applicationID: snowflake,
-  coverImage: string
-) =>
-  `https://cdn.discordapp.com/app-icons/${applicationID}/${coverImage}.png` as const;
-export const applicationIcon = (applicationID: snowflake, icon: string) =>
-  `https://cdn.discordapp.com/app-icons/${applicationID}/${icon}.png` as const;
-export const customEmoji = (emojiID: snowflake) =>
-  `https://cdn.discordapp.com/emojis/${emojiID}.png` as const;
+  coverImage: string,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP = ImageFormats.PNG
+) => cdnURL(`app-icons/${applicationID}/${coverImage}`, imageFormat);
+export const applicationIcon = (
+  applicationID: snowflake,
+  icon: string,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP = ImageFormats.PNG
+) => cdnURL(`app-icons/${applicationID}/${icon}`, imageFormat);
+export const customEmoji = (
+  emojiID: snowflake,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP
+    | ImageFormats.GIF = ImageFormats.PNG
+) => cdnURL(`emojis/${emojiID}`, imageFormat);
 export const defaultUserAvatar = (index: string) =>
-  `https://cdn.discordapp.com/embed/avatars/${index}.png` as const;
-export const guildBanner = (guildID: snowflake, banner: string) =>
-  `https://cdn.discordapp.com/banners/${guildID}/${banner}.png` as const;
-export const guildDiscoverySplash = (guildID: snowflake, splash: string) =>
-  `https://cdn.discordapp.com/discovery-splashes/${guildID}/${splash}.png` as const;
-export const guildIcon = (guildID: snowflake, icon: string) =>
-  `https://cdn.discordapp.com/icons/${guildID}/${icon}.png` as const;
+  cdnURL(`embed/avatars/${index}`);
+export const guildBanner = (
+  guildID: snowflake,
+  banner: string,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP
+    | ImageFormats.GIF = ImageFormats.PNG
+) => cdnURL(`banners/${guildID}/${banner}`, imageFormat);
+export const guildDiscoverySplash = (
+  guildID: snowflake,
+  splash: string,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP = ImageFormats.PNG
+) => cdnURL(`discovery-splashes/${guildID}/${splash}`, imageFormat);
+export const guildIcon = (
+  guildID: snowflake,
+  icon: string,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP
+    | ImageFormats.GIF = ImageFormats.PNG
+) => cdnURL(`icons/${guildID}/${icon}`, imageFormat);
 export const guildMemberAvatar = (
   guildID: snowflake,
   userID: snowflake,
-  avatar: string
-) =>
-  `https://cdn.discordapp.com/guilds/${guildID}/users/${userID}/avatars/${avatar}.png` as const;
+  avatar: string,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP
+    | ImageFormats.GIF = ImageFormats.PNG
+) => cdnURL(`guilds/${guildID}/users/${userID}/avatars/${avatar}`, imageFormat);
 export const guildMemberBanner = (
   guildID: snowflake,
   userID: snowflake,
-  banner: string
-) =>
-  `https://cdn.discordapp.com/guilds/${guildID}/users/${userID}/banners/${banner}.png` as const;
+  banner: string,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP
+    | ImageFormats.GIF = ImageFormats.PNG
+) => cdnURL(`guilds/${guildID}/users/${userID}/banners/${banner}`, imageFormat);
 export const guildScheduledEventCover = (
   scheduledEventID: snowflake,
-  coverImage: string
-) =>
-  `https://cdn.discordapp.com/guild-events/${scheduledEventID}/${coverImage}.png` as const;
-export const guildSplash = (guildID: snowflake, splash: string) =>
-  `https://cdn.discordapp.com/splashes/${guildID}/${splash}.png` as const;
-export const roleIcon = (roleID: snowflake, icon: string) =>
-  `https://cdn.discordapp.com/role-icons/${roleID}/${icon}.png` as const;
-export const stickerPackBanner = (assetID: snowflake) =>
-  `https://cdn.discordapp.com/app-assets/710982414301790216/store/${assetID}.png` as const;
-export const sticker = (stickerID: snowflake) =>
-  `https://cdn.discordapp.com/stickers/${stickerID}.png` as const;
-export const storePageAsset = (applicationID: snowflake, assetID: snowflake) =>
-  `https://cdn.discordapp.com/app-assets/${applicationID}/store/${assetID}` as const;
-export const teamIcon = (teamID: snowflake, icon: string) =>
-  `https://cdn.discordapp.com/team-icons/${teamID}/${icon}.png` as const;
-export const userAvatar = (userID: snowflake, avatar: string) =>
-  `https://cdn.discordapp.com/avatars/${userID}/${avatar}.png` as const;
+  coverImage: string,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP = ImageFormats.PNG
+) => cdnURL(`guild-events/${scheduledEventID}/${coverImage}`, imageFormat);
+export const guildSplash = (
+  guildID: snowflake,
+  splash: string,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP = ImageFormats.PNG
+) => cdnURL(`splashes/${guildID}/${splash}`, imageFormat);
+export const roleIcon = (
+  roleID: snowflake,
+  icon: string,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP = ImageFormats.PNG
+) => cdnURL(`role-icons/${roleID}/${icon}`, imageFormat);
+export const stickerPackBanner = (
+  assetID: snowflake,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP = ImageFormats.PNG
+) => cdnURL(`app-assets/710982414301790216/store/${assetID}`, imageFormat);
+export const sticker = (
+  stickerID: snowflake,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.Lottie
+    | ImageFormats.GIF = ImageFormats.PNG
+) => cdnURL(`stickers/${stickerID}`, imageFormat);
+export const storePageAsset = (
+  applicationID: snowflake,
+  assetID: snowflake,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP = ImageFormats.PNG
+) => cdnURL(`app-assets/${applicationID}/store/${assetID}`, imageFormat);
+export const teamIcon = (
+  teamID: snowflake,
+  icon: string,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP = ImageFormats.PNG
+) => cdnURL(`team-icons/${teamID}/${icon}`, imageFormat);
+export const userAvatar = (
+  userID: snowflake,
+  avatar: string,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP
+    | ImageFormats.GIF = ImageFormats.PNG
+) => cdnURL(`avatars/${userID}/${avatar}`, imageFormat);
 export const userAvatarDecoration = (
   userID: snowflake,
   avatarDecoration: string
-) =>
-  `https://cdn.discordapp.com/avatar-decorations/${userID}/${avatarDecoration}.png` as const;
-export const userBanner = (userID: snowflake, banner: string) =>
-  `https://cdn.discordapp.com/banners/${userID}/${banner}.png` as const;
+) => cdnURL(`avatar-decorations/${userID}/${avatarDecoration}`);
+export const userBanner = (
+  userID: snowflake,
+  banner: string,
+  imageFormat:
+    | ImageFormats.PNG
+    | ImageFormats.JPEG
+    | ImageFormats.JPG
+    | ImageFormats.WebP
+    | ImageFormats.GIF = ImageFormats.PNG
+) => cdnURL(`banners/${userID}/${banner}`, imageFormat);
