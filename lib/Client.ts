cb341c22685b6f77cf714e0904eb4e7e925f254f
XenKys <sergiotuzzo@gmail.com>
2024-07-07 19:00:52 +0200

    fix: 'editAutoModerationRule'

--- lib/Client.ts
+++ lib/Client.ts
@@ -1775,7 +1775,19 @@ export class Client extends EventEmitter {
           name: options.name,
           event_type: options.eventType,
           trigger_type: options.triggerType,
-          trigger_metadata: options.triggerMetadata,
+          trigger_metadata:
+            options.triggerMetadata !== undefined
+              ? {
+                  keyword_filter: options.triggerMetadata.keywordFilter,
+                  regex_patterns: options.triggerMetadata.regexPatterns,
+                  presets: options.triggerMetadata.presets,
+                  allow_list: options.triggerMetadata.allowList,
+                  mention_total_limit:
+                    options.triggerMetadata.mentionTotalLimit,
+                  mention_raid_protection:
+                    options.triggerMetadata.mentionRaidProtection,
+                }
+              : undefined,
           actions: options.actions?.map((action) => ({
             type: action.type,
             metadata: {
