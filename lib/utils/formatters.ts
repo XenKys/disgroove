55d26cb56c6ba759fa508bb4d58a1a1e2aac0bc1
XenKys <sergiotuzzo@gmail.com>
2024-07-08 10:08:17 +0200

    refactor: added docs links to formatters

--- lib/utils/formatters.ts
+++ lib/utils/formatters.ts
@@ -1,20 +1,22 @@
-/* https://discord.com/developers/docs/reference#message-formatting */
-
 import type { GuildNavigationTypes, TimestampStyles } from "../constants";
 import type { snowflake } from "../types/common";
 
+/** https://discord.com/developers/docs/reference#message-formatting-formats */
 export function userMention(userID: snowflake): string {
   return `<@${userID}>`;
 }
 
+/** https://discord.com/developers/docs/reference#message-formatting-formats */
 export function channelMention(channelID: snowflake): string {
   return `<#${channelID}>`;
 }
 
+/** https://discord.com/developers/docs/reference#message-formatting-formats */
 export function roleMention(roleID: snowflake): string {
   return `<@&${roleID}>`;
 }
 
+/** https://discord.com/developers/docs/reference#message-formatting-formats */
 export function slashCommandMention(
   commandName: string,
   commandID: snowflake,
@@ -28,6 +30,7 @@ export function slashCommandMention(
     : `</${commandName}:${commandID}>`;
 }
 
+/** https://discord.com/developers/docs/reference#message-formatting-formats */
 export function customEmoji(
   emojiName: string,
   emojiID: snowflake,
@@ -38,10 +41,12 @@ export function customEmoji(
     : `<:${emojiName}:${emojiID}>`;
 }
 
+/** https://discord.com/developers/docs/reference#message-formatting-formats */
 export function unixTimestamp(time: number, style?: TimestampStyles): string {
   return style ? `<t:${time}:${style}>` : `<t:${time}>`;
 }
 
+/** https://discord.com/developers/docs/reference#message-formatting-formats */
 export function guildNavigation(
   guildID: snowflake,
   type: GuildNavigationTypes
