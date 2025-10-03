a4c141fa51604113a3a357dfd835dcdccd2b6f94
XenKys <sergiotuzzo@gmail.com>
2025-10-03 10:16:43 +0200

    refactor: ID instead of Id in constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1274,8 +1274,8 @@ export enum JSONErrorCodes {
   FailedToCreateStageNeededForStageEvent = 180002,
   MessageWasBlockedByAutomaticModeration = 200000,
   TitleWasBlockedByAutomaticModeration,
-  WebhooksPostedToForumChannelsMustHaveAThreadNameOrThreadId = 220001,
-  WebhooksPostedToForumChannelsCannotHaveBothAThreadNameAndThreadId,
+  WebhooksPostedToForumChannelsMustHaveAThreadNameOrThreadID = 220001,
+  WebhooksPostedToForumChannelsCannotHaveBothAThreadNameAndThreadID,
   WebhooksCanOnlyCreateThreadsInForumChannels,
   WebhookServicesCannotBeUsedInForumChannels,
   MessageBlockedByHarmfulLinksFilter = 240000,
@@ -1301,7 +1301,7 @@ export enum RPCErrorCodes {
   InvalidEvent,
   InvalidChannel,
   InvalidPermissions,
-  InvalidClientId,
+  InvalidClientID,
   InvalidOrigin,
   InvalidToken,
   InvalidUser,
@@ -1314,7 +1314,7 @@ export enum RPCErrorCodes {
 
 /** https://discord.com/developers/docs/topics/opcodes-and-status-codes#rpc-rpc-close-event-codes */
 export enum RPCCloseEventCodes {
-  InvalidClientId = 4000,
+  InvalidClientID = 4000,
   InvalidOrigin,
   RateLimited,
   TokenRevoked,
